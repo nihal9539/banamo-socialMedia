689499b48a20c52220cbf98cad254e7f9ebded1b
Mohammed Nihal <mohammednihal691@gmail.com>
2024-02-09 21:51:39 +0530

    update

--- client/src/api/PostRequest.js
+++ client/src/api/PostRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API = axios.create({baseURL:"http://localhost:3000"})
+const API = axios.create({baseURL:"https://banamo-social-media-7b1g.vercel.app/"})  
 
 export const uploadPost = (data )=> API.post('/post',data)
 export const getAllPost =async ()=> API.get(`/post`)
